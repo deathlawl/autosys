a19b6b3ce2bf1818b0336e68252a5ce9d550716b
deathlawl <ifhm2002@gmail.com>
2020-03-03 16:51:32 -0500

    new file:   commands/send.js

---
+++ commands/send.js
@@ -0,0 +1,16 @@
+module.exports = {
+	name: 'send',
+	description: 'generates coordinates if they do not already exist, and saves them',
+	execute(message, args) {
+       
+    //gets the data from the coordinate file
+    ffs.readFile('./coordinates.json', (err, jsonString) => {
+        if (err) {
+            console.log("File read failed:", err)
+            message.channel.send(`Whoops, can't seem to find any of the coordinates, ${message.author}`)
+            return
+        }
+        console.log('File data:', jsonString)
+    })
+}
+}
